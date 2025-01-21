ed755452478a7305264dd1b1d721933b3c32fc71
mkr_msk <i@makarov-podolsk.ru>
2025-01-21 23:30:14 +0300

    - добавлено inline-меню Update

--- TaskRandomizer.py
+++ TaskRandomizer.py
@@ -8,11 +8,11 @@ from Elements import *
 # Библиотеки для AIOgram
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command, CommandObject
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 # Логирование
 logging.basicConfig(level=logging.INFO)
@@ -34,10 +34,7 @@ async def cmd_start(message: types.Message):
         types.KeyboardButton(text="/next"),
         types.KeyboardButton(text="/show"),
     )
-    builder.row(types.KeyboardButton(text='/update Default'))
-    builder.row(types.KeyboardButton(text='/update Active Chores 0'))
-    builder.row(types.KeyboardButton(text='/update Active Hygiene 0'))
-    builder.row(types.KeyboardButton(text='/update Active Workout 0'))
+    builder.row(types.KeyboardButton(text='/update_menu'))
 
     await message.answer(
         text='Select action:',
@@ -68,7 +65,7 @@ async def cmd_show(message: types.Message):
 
 # Обработка add
 @dp.message(Command('add'))
-async def cmd_add(message: types.Message, command: CommandObject):
+async def cmd_add(command: CommandObject):
     e = Element()
     args = command.args.split()
 
@@ -80,25 +77,81 @@ async def cmd_add(message: types.Message, command: CommandObject):
 
 # Обработка update
 @dp.message(Command('update'))
-async def cmd_update(message: types.Message, command: CommandObject):
+async def cmd_update(command: CommandObject):
     args = command.args.split()
     target = args[0]
+    name = args[1]
+    new_value = args[2]
+    update_item(target, name, new_value)
 
-    if target == 'Default':
-        update_item(target)
+# Обработка update_menu
+@dp.message(Command('update_menu'))
+async def cmd_update_menu(message: types.Message):
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Reset all",
+        callback_data="update_default")
+    )
+    builder.add(types.InlineKeyboardButton(
+        text="Deactivate",
+        callback_data="update_active")
+    )
 
-    else:
-        name = args[1]
-        new_value = args[2]
-        update_item(target, name, new_value)
+    await message.answer(
+        text="Select update mode:",
+        reply_markup=builder.as_markup()
+    )
 
 # Обработка delete
 @dp.message(Command('delete'))
-async def cmd_delete(message: types.Message, command: CommandObject):
+async def cmd_delete(command: CommandObject):
     args = command.args.split()
     name = args[0]
 
     delete_item(name)
+
+# Обработка update_menu / update_default
+@dp.callback_query(F.data == "update_default")
+async def update_default():
+    update_item('Default')
+
+# Обработка update_menu / update_active
+@dp.callback_query(F.data == "update_active")
+async def update_active(callback: types.CallbackQuery):
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Chores",
+        callback_data="update_active_chores")
+    )
+    builder.add(types.InlineKeyboardButton(
+        text="Hygiene",
+        callback_data="update_active_hygiene")
+    )
+    builder.add(types.InlineKeyboardButton(
+        text="Workout",
+        callback_data="update_active_workout")
+    )
+
+    await callback.message.answer(
+        text='Select item to deactivate:',
+        reply_markup=builder.as_markup(resize_keyboard=True),
+    )
+
+# Обработка update_menu / update_active / update_active_chores
+@dp.callback_query(F.data == "update_active_chores")
+async def update_active_chores():
+    update_item('Active', 'Chores', '0')
+
+# Обработка update_menu / update_active / update_active_hygiene
+@dp.callback_query(F.data == "update_active_hygiene")
+async def update_active_hygiene():
+    update_item('Active', 'Hygiene', '0')
+
+# Обработка update_menu / update_active / update_active_workout
+@dp.callback_query(F.data == "update_active_workout")
+async def update_active_workout():
+    update_item('Active', 'Workout', '0')
+
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
